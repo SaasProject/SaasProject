450f5a09c8de6972dbc482324668a270e8645688
Macku Sanchez <mackusanchez@gmail.com>
2017-12-12 17:46:32 +0800

    fixed minor bug

--- app/account/index.controller.js
+++ app/account/index.controller.js
@@ -22,17 +22,23 @@
         }
  
         function saveUser() {
-
-            if(vm.user.password != vm.user.confirmPassword){
-                FlashService.Error("Password doesn't match");
+            if(vm.user.password===undefined){
+                console.log(vm.user.password);
+                FlashService.Error("Enter New Password");
             }else{
-                UserService.Update(vm.user)
-                    .then(function () {
-                        FlashService.Success('User updated');
-                    })
-                    .catch(function (error) {
-                        FlashService.Error(error);
-                    });
+                if(vm.user.password != vm.user.confirmPassword){
+                    console.log('wew');
+                    console.log(vm.user.password);
+                    FlashService.Error("Password doesn't match");
+                }else{
+                    UserService.Update(vm.user)
+                        .then(function () {
+                            FlashService.Success('User updated');
+                        })
+                        .catch(function (error) {
+                            FlashService.Error(error);
+                        });
+                }
             }
         }
  
