2e18aa490a13735af24adafeae1ee3fd5b97e898
jano-omugtong <34333390+jano-omugtong@users.noreply.github.com>
2017-12-12 10:39:42 +0800

    updated server.js

--- server.js
+++ server.js
@@ -5,31 +5,32 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var expressJwt = require('express-jwt');
 var config = require('config.json');
-
+ 
 //added by jeremy
 require('./models/models');
 var mongoose = require('mongoose');
 mongoose.connect(config.connectionString);
- 
+
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({ secret: config.secret, resave: false, saveUninitialized: true }));
  
-// use JWT auth to secure the api
-app.use('/api', expressJwt({ secret: config.secret }).unless({ path: ['/api/users/authenticate', '/api/users/register'] }));
+// use JWT auth to secure the api   // edited by dyan0: added '/api/users/emailOn'
+app.use('/api', expressJwt({ secret: config.secret }).unless({ path: ['/api/users/authenticate', '/api/users/register', '/api/users/emailOn'] }));
  
 // routes
 app.use('/login', require('./controllers/login.controller'));
 app.use('/register', require('./controllers/register.controller'));
 app.use('/app', require('./controllers/app.controller'));
 app.use('/api/users', require('./controllers/api/users.controller'));
-
+ 
 //added by jeremy
 app.use('/api/assets', require('./controllers/api/assets.controller'));
+// added by dyan0
+app.use('/forgotPass', require('./controllers/forgotPass.controller'));
 
- 
 // make '/app' default route
 app.get('/', function (req, res) {
     return res.redirect('/app');
