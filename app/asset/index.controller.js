c9ed414b6b5c5c80dc5a1bfbe6afcb39f3f79539
jeremybreccion <34333326+jeremybreccion@users.noreply.github.com>
2017-12-14 15:00:02 +0800

    new filename for report

--- app/asset/index.controller.js
+++ app/asset/index.controller.js
@@ -14,7 +14,7 @@
 
 
  
-    function Controller($window, AssetService, FlashService, $scope) {
+    function Controller($window, AssetService, FlashService, $scope, $interval, $filter) {
  
         //initialization
         $scope.newAsset = {
@@ -28,30 +28,22 @@
         $scope.currentPage = 1;
         $scope.pageSize = 10;
         
-        var fulldate = new Date();
-        var date = fulldate.getFullYear() + "-" + fulldate.getMonth() + "-" + fulldate.getDate();
-        $scope.filename = "Report " + date;
+        $scope.$watch(function(){
+            $interval(function(){
+                $scope.filename = "Report " + $filter('date')(new Date(), "yyyy-MM-dd h:mma");
+            }, 1000);
+        });
+
 
         //get all assets
         AssetService.GetAll().then(function(assets){
             if(assets.length > 0){               
-
                 //store to array
                 $scope.assets = assets;
 
                 //get the fields of assets. since it is assumed that schema is fixed, you can get fields on any object
                 //keys = fields
                 $scope.columns = Object.keys($scope.assets[0]);
-
-                //value is the current asset in assets, key is index
-                angular.forEach($scope.assets, function(value, key){
-
-                    //check if warehouse already exists. if yes, don't add.
-                    //this is to eliminate duplicates in datalist
-                    if($scope.warehouses.indexOf(value.warehouse) == -1){
-                        $scope.warehouses.push(value.warehouse);
-                    } 
-                });
             }
             else{
                 //perform notification here
