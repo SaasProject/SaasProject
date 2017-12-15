f02e94a3a1909ed2d42f861e165168bce7ebb354
jenny-ayala <34356624+jenny-ayala@users.noreply.github.com>
2017-12-15 13:28:43 +0800

    edited the delete function

--- app/manageUsers/index.controller.js
+++ app/manageUsers/index.controller.js
@@ -180,21 +180,25 @@
 		//deleteUser function
 		vm.deleteUser = function(index) {
 			
-			var toDel = vm.user[index];
+			
+			 var toDel = filterIndexById($scope.allUsers, index);
+        
+			
+			console.log(toDel);
 
-            if (confirm("Are you sure to delete this user?")){
+            // if (confirm("Are you sure to delete this user?")){
 				
-             UserService.Delete(toDel._id)
-                 .then(function () {
+             // UserService.Delete(toDel._id)
+                 // .then(function () {
 					
-					FlashService.Success('User Deleted');
-					initController();
+					// FlashService.Success('User Deleted');
+					// initController();
 					 
-                })
-                .catch(function (error) {
-                    FlashService.Error(error);
-                });
-            }
+                // })
+                // .catch(function (error) {
+                    // FlashService.Error(error);
+                // });
+            // }
         }
     }
  
