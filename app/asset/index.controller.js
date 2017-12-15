3c4e1125974373ca5ac2d90618e2a43c4382db4e
jeremybreccion <34333326+jeremybreccion@users.noreply.github.com>
2017-12-15 08:26:53 +0800

    minor fix to asset controller

--- app/asset/index.controller.js
+++ app/asset/index.controller.js
@@ -32,30 +32,35 @@
             return "Report " + $filter('date')(new Date(), "yyyy-MM-dd h:mma");
         };
 
+        function getAllAssets(){
+            //get all assets
+            AssetService.GetAll().then(function(assets){
+                if(assets.length > 0){               
+                    //store to array
+                    $scope.assets = assets;
 
-        //get all assets
-        AssetService.GetAll().then(function(assets){
-            if(assets.length > 0){               
-                //store to array
-                $scope.assets = assets;
-
-                //get the fields of assets. since it is assumed that schema is fixed, you can get fields on any object
-                //keys = fields
-                $scope.columns = Object.keys($scope.assets[0]);
-            }
-            else{
+                    //get the fields of assets. since it is assumed that schema is fixed, you can get fields on any object
+                    //keys = fields
+                    $scope.columns = Object.keys($scope.assets[0]);
+                }
+                else{
+                    //perform notification here
+                    FlashService.Error("No assets found");
+                }
+            }, function(){
                 //perform notification here
-                FlashService.Error("No assets found");
-            }
-        }, function(){
-            //perform notification here
-            FlashService.Error("An error occurred");
-        });
+                FlashService.Error("An error occurred");
+            });
+        }
+
+        getAllAssets();
+        
 
         $scope.addAsset = function(){
             console.log($scope.newAssets);
             AssetService.addAsset($scope.newAssets)
                     .then(function () {
+                        getAllAssets();
                         FlashService.Success('Asset Added');
                     })
                     .catch(function (error) {
